cd62c18c9ec03a256c0e253544fbf74346547e84
mos0901386749 <99708987+mos0901386749@users.noreply.github.com>
2022-02-22 10:41:28 +0700

    update

---
+++ Test Suites/LoginDataDriven.ts
@@ -0,0 +1,41 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<TestSuiteEntity>
+   <description></description>
+   <name>LoginDataDriven</name>
+   <tag></tag>
+   <isRerun>false</isRerun>
+   <mailRecipient></mailRecipient>
+   <numberOfRerun>0</numberOfRerun>
+   <pageLoadTimeout>30</pageLoadTimeout>
+   <pageLoadTimeoutDefault>true</pageLoadTimeoutDefault>
+   <rerunFailedTestCasesOnly>false</rerunFailedTestCasesOnly>
+   <rerunImmediately>false</rerunImmediately>
+   <testSuiteGuid>2f281712-8a02-4c9b-a03a-fb1f0ad37d09</testSuiteGuid>
+   <testCaseLink>
+      <guid>3a9f70eb-034a-4b50-8349-54f8aa37895f</guid>
+      <isReuseDriver>false</isReuseDriver>
+      <isRun>true</isRun>
+      <testCaseId>Test Cases/UC 101 Login (Sucess-DataDrivenlesting)</testCaseId>
+      <testDataLink>
+         <combinationType>ONE</combinationType>
+         <id>f339f7b2-f337-4d65-a5f8-5aa3c28d7acc</id>
+         <iterationEntity>
+            <iterationType>ALL</iterationType>
+            <value></value>
+         </iterationEntity>
+         <testDataId>Data Files/LoginData</testDataId>
+      </testDataLink>
+      <variableLink>
+         <testDataLinkId>f339f7b2-f337-4d65-a5f8-5aa3c28d7acc</testDataLinkId>
+         <type>DATA_COLUMN</type>
+         <value>username</value>
+         <variableId>8f585066-f632-4bf6-961d-16b3ad6293c0</variableId>
+      </variableLink>
+      <variableLink>
+         <testDataLinkId>f339f7b2-f337-4d65-a5f8-5aa3c28d7acc</testDataLinkId>
+         <type>DATA_COLUMN</type>
+         <value>password</value>
+         <variableId>321f49d5-f1e1-4ded-9266-f0668b12e796</variableId>
+      </variableLink>
+   </testCaseLink>
+</TestSuiteEntity>
